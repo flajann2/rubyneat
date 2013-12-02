8a859046bbb473f9aca20428090f6429cb1e98cf
Fred Mitchell <fred_github@linuxbloke.com>
2013-12-02 15:56:29 -0500

    command line already

--- bin/neat.rb
+++ bin/neat.rb
@@ -4,6 +4,37 @@
 
 =end
 
+require 'pp'
+require 'slop'
+
 # Be sure lib is added to the library path
 $:.unshift File.join( %w{ lib } )
 
+opts = Slop.parse do
+  banner 'Usage: neat [commands] [options] ...'
+
+  on :version, 'Version information' do
+    puts 'v0.0.0'
+  end
+
+  command :console do
+    
+  end
+
+  command :list do
+
+  end
+
+  command :run do
+    on :v, :verbose, 'Verbose mode'
+
+    run do |opts, args|
+      pp args
+    end
+  end
+
+  run do |opts, args|
+    puts args
+    puts opts
+  end
+end
