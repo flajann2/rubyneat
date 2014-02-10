b4acd6731724c79cd513b561588344aefa002f4c
Fred Mitchell <fred_github@linuxbloke.com>
2014-02-10 07:09:15 -0500

    centrifugal forces left out of the mix for now

--- neater/rnlib/inverted_pendulum.rb
+++ neater/rnlib/inverted_pendulum.rb
@@ -24,6 +24,7 @@ module InvertedPendulum
   GC = 6.67384e-11 # m3 kg-1 s-2
   ADTG = -9.81 # m/s**2, acceleration due to gravity on earth
   TORAD = PI / 180.0 # converts degrees to radians, deg * TORAD
+  PI2 = 2.0 * PI
 
   # The following are array indicies of the vector.
   X = 0 # Horizontal, cart travels in this coordinate.
@@ -79,14 +80,31 @@ module InvertedPendulum
   class Cart
     attr_accessor :pix_meters # pixels per meter
 
-    def initialize(ipwin, scale: 0.50)
+    #@param ipwin -- the windowed canvalss this will be shown.
+    #@param scale -- visual scale on the canvas.
+    #@param ang   -- initial angle in degrees, 0 being the
+    #                positive side of the x axis.
+    #@param xpos  -- initial x position of the center of cart, in pixels.
+    #@param cartmass -- in kg. included are the mass of the wheels.
+    #                   We will not deal with the angular momentum
+    #                   of the wheels, because that's beyond the scope
+    #                   of what this is supposed to accomplish.
+    #@param polemass -- in kg. The mass of the pole is assumed to all
+    #                   reside at a point at
+    #                   the knobby end.
+    def initialize(ipwin, scale: 0.50,
+          ang: 90.1,
+          xpos: 500.0,
+          cartmass: 200.0,
+          polemass: 100.10)
+      @t = 0
       @scale = scale
       @cart_length = 5.0 # meters
       @pix_meters = 640.0 * scale / @cart_length
       @ipwin = ipwin
       @cart = {
           image: Gosu::Image.new(ipwin, 'public/cart.png', true),
-          pos: Vector[1000 / @pix_meters, 650 / @pix_meters, 0],
+          pos: Vector[xpos / @pix_meters, 650 / @pix_meters, 0],
           vel: Vector[0.0, 0, 0], #speed in meters per second
           acc: Vector[0.0, 0, 0], #acceleration in meters per second squared
           scale: 0.2 * scale,
@@ -96,10 +114,7 @@ module InvertedPendulum
               horiz: Vector[1.0, 0.0, 0.0], # The cart is resticted to movement on the x axis
               vert: Vector[0.0, 1.0, 0.0]}, # so these basis vectors will never change.
           force: {},
-          mass: 100.0 # in kg. included are the mass of the wheels.
-                      # We will not deal with the angular momentum of the wheels,
-                      # because that's beyond the scope of what this is supposed to
-                      # accomplish.
+          mass: cartmass
       }
       @cart[:length] = @cart[:image].width * @cart[:scale] / @pix_meters
       @cart[:height] = @cart[:image].height * @cart[:scale] / @pix_meters
@@ -110,15 +125,17 @@ module InvertedPendulum
           z: 0,
           xoff: 0.0,
           yoff: 0.5,
-          ang: 45, # angle is in degrees
+          ang: ang, # angle is in degrees
           dang: 0.0, # degrees per second
           ddang: nil, # angular acceleration, degree / second ** 2
+          cdang: 0.0, # degrees per second, contribution from cart
+          cddang: nil, # angular acceleration, degree / second ** 2, contribution from cart
           scale: 0.2 * scale,
           force: {}, # shall hold the 2 force vectors :shaft and :radial
           basis: {}, # basis (unit) vectors
+          cacc: Vector[0,0,0], # acceleration from cart
           length: nil, # in meters, calculated from the scaled pixel length.
-          mass: 100.0 # in kg. The mass of the pole is assumed to all reside at a point at
-                      # the knobby end.
+          mass: polemass
       }
       @pole[:length] = @pole[:image].width * @pole[:scale] / @pix_meters
 
@@ -151,12 +168,14 @@ module InvertedPendulum
     def update
       ## Physics updates
       @dt = @ipwin.update_interval / 1000.0
+      @t += @dt
 
       ## Pole (Pendulum) forces, accelerations, etc.
       # basis vectors
       ang = @pole[:ang] * TORAD
-      @pole[:basis][:ishaft] = iShaft   = Vector[cos(ang), sin(ang),  0]
-      @pole[:basis][:iradial] = iRadial = Vector[sin(ang), -cos(ang), 0]
+      @pole[:time] = @t
+      @pole[:basis][:shaft] = iShaft   = Vector[cos(ang), sin(ang),  0]
+      @pole[:basis][:radial] = iRadial = Vector[sin(ang), -cos(ang), 0]
       @pole[:r] = r = iShaft * @pole[:length]
       @pole[:force][:shaft] = iShaft.basis(GV * @pole[:mass])
       @pole[:force][:radial] = radial = iRadial.basis(GV * @pole[:mass])
@@ -164,27 +183,43 @@ module InvertedPendulum
       @pole[:force][:torque] = torque = r.cross_product(radial)
       @pole[:alpha] = alpha = torque / (@pole[:mass] * (@pole[:length] ** 2.0))
       @pole[:ddang] = -alpha[Z] / TORAD # the pseudo vector component Z is the signed magnitude
-      @pole[:dang] += @pole[:ddang] * @dt
+      @pole[:dang] += @pole[:ddang] * @dt # angular velocity
+      @pole[:vel] = @pole[:basis][:radial] * (@pole[:dang] * TORAD * PI2 * @pole[:length]) #linear velocity at the pole mass
       @pole[:ang] += @pole[:dang] * @dt
 
-      ## Cart forces from pole [:force][:shaft]
-      @cart[:force][:shaft] = shaft = @pole[:force][:shaft]
+      ## Cart forces from pole [:force][:shaft] and centifugal
+      @cart[:time] = @t
+
+      @pole[:force][:centrifugal] = centrifugal = (@pole[:basis][:shaft] * @pole[:vel].magnitude ** 2) * @pole[:mass]
+      @cart[:force][:shaft] = shaft = @pole[:force][:shaft] # + centrifugal #FIXME cemtrifugal calculations wonky!
       @cart[:force][:horiz] = horiz = @cart[:basis][:horiz].basis shaft
       @cart[:force][:vert] = @cart[:basis][:vert].basis shaft # not that we use the vert
 
       # Now the horz force induces an acceleration on the cart.
       # Any additions to the acceleration vector must be done after
       # this point.
-      @cart[:acc] = horiz / @cart[:mass]
+      @cart[:acc] = horiz / (@cart[:mass] + @pole[:mass] * cos(ang).abs)
 
-      # Cart acceleration also affects angular torque
-      # TODO back transfer
+      ## Cart acceleration also affects angular torque
+      # FIXME: Note that recalculations are being done here, which
+      # FIXME: are not DRY. Redo this properly later.
+      @pole[:cacc] = cacc = @cart[:acc] * -1.0
+      @pole[:force][:cradial] = radial = iRadial.basis(cacc * @pole[:mass])
+      # the magnitude of the radial vector goes to the torque on
+      @pole[:force][:ctorque] = torque = r.cross_product(radial)
+      @pole[:calpha] = alpha = torque / (@pole[:mass] * (@pole[:length] ** 2.0))
+      @pole[:cddang] = -alpha[Z] / TORAD # the pseudo vector component Z is the signed magnitude
+      @pole[:cdang] += @pole[:cddang] * @dt
+      @pole[:ang] += @pole[:cdang] * @dt
 
-      # actual cart physics
+      ## Actual cart physics
       @cart[:vel][X] += @cart[:acc][X] * @dt
       @cart[:pos][X] += @cart[:vel][X] * @dt
       @cart[:x] = @cart[:pos][X] * @pix_meters
       @cart[:y] = @cart[:pos][Y] * @pix_meters
+
+      puts '=' * 80
+      pp({pole: @pole, cart: @cart})
 
       # wheels physics -- angular velocity of each wheel based
       # on the linear velocity of the cart.
