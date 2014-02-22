56e08fc001eb4a92d635f86322ab86fbc1b6273f
Fred Mitchell <fred_github@linuxbloke.com>
2014-02-22 15:39:29 -0500

    setting up RubyNEAT to evolve for polebalancing

--- neater/invpend_neat.rb
+++ neater/invpend_neat.rb
@@ -29,3 +29,70 @@ invpend do |ipwin|
 
   show cart: c
 end
+
+define "InvPend System" do
+  #-----------------------------------------------------
+  #= Neuron Specifications
+  inputs(
+    in_cart_velocity: InputNeuron,
+    in_cart_position: InputNeuron,
+    in_pole_velocity: InputNeuron,
+    in_pole_angle:    InputNeuron,
+    bias: BiasNeuron
+  )
+
+  outputs out_bang_left: TanhNeuron, out_bang_right: TanhNeuron
+
+  hidden tan: TanhNeuron
+
+  #-----------------------------------------------------
+  #= Settings
+  # General
+  hash_on_fitness false
+  start_population_size 30
+  population_size 30
+  max_generations 10000
+  max_population_history 10
+
+  #-----------------------------------------------------
+  #= Evolver probabilities and SDs
+  # Perturbations
+  mutate_perturb_gene_weights_prob 0.10
+  mutate_perturb_gene_weights_sd 0.25
+
+  # Complete Change of weight
+  mutate_change_gene_weights_prob 0.10
+  mutate_change_gene_weights_sd 1.00
+
+  # Adding new neurons and genes
+  mutate_add_neuron_prob 0.05
+  mutate_add_gene_prob 0.20
+
+  # Switching genes on and off
+  mutate_gene_disable_prob 0.01
+  mutate_gene_reenable_prob 0.01
+
+  interspecies_mate_rate 0.03
+  mate_only_prob 0.10 #0.7
+
+  # Mating
+  survival_threshold 0.20 # top % allowed to mate in a species.
+  survival_mininum_per_species  4 # for small populations, we need SOMETHING to go on.
+
+  # Fitness costs
+  fitness_cost_per_neuron 0#.00001
+  fitness_cost_per_gene   0#.00001
+
+  # Speciation
+  compatibility_threshold 2.5
+  disjoint_coefficient 0.6
+  excess_coefficient 0.6
+  weight_coefficient 0.2
+  max_species 20
+  dropoff_age 15
+  smallest_species 5
+
+  # Sequencing
+  start_sequence_at 0
+  end_sequence_at 200
+end
