6eec7a042c5f8ccce8d2710344606f478bd6578d
Fred Mitchell <fred_github@linuxbloke.com>
2014-02-23 20:03:34 -0500

    scaffolding code in for invpend

--- neater/invpend_neat.rb
+++ neater/invpend_neat.rb
@@ -96,3 +96,55 @@ define "InvPend System" do
   start_sequence_at 0
   end_sequence_at 200
 end
+
+evolve do
+  # Each sequence query represents a time instant
+  # for evaluating the inverse pendulum.
+  #
+  # For this, we use a named vector (just a Hash, really),
+  # so we can ensure that the input parameters are lined up with
+  # the appropriate input neurons. When using named vectors,
+  # any non-matches are ignored, allowing you to play around
+  # with changes in input neurons with ease.
+  query { |seq|
+    # TODO: do an interation with the cart and return results.
+    # FIXME: how do we know this is a new sequence? I suppose we can
+    # FIXME: tell that the start number has been hit again.
+    {
+      in_cart_velocity: 0,
+      in_cart_position: 0,
+      in_pole_velocity: 0,
+      in_pole_angle: 0
+    }
+  }
+
+  # Compare the fitness of two critters.
+  # We may choose a different ordering here.
+  compare {|f1, f2| f2 <=> f1 }
+
+  # Here we integrate the cost with the fitness.
+  cost { |fitvec, cost|
+    #fit = XOR_STATES - fitvec.reduce {|a,r| a+r} - cost
+    #$log.debug ">>>>>>> fitvec #{fitvec} => #{fit}, cost #{cost}"
+    #fit
+  }
+
+  fitness { |vin, vout, seq|
+  }
+
+  stop_on_fitness {|fitness, c|
+    puts "*** Generation Run #{c.generation_num}, best is #{fitness[:best]} ***\n\n"
+    fitness[:best] >= ALMOST_FIT
+  }
+end
+
+report do |rept|
+  $log.info "REPORT #{rept.to_yaml}"
+end
+
+# The block here is called upon the completion of each generation
+run_engine do |c|
+  $log.info "******** Run of generation %s completed, history count %d ********" %
+                [c.generation_num, c.population_history.size]
+end
+
