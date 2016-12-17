410b1968748376974895baf36b0c6c87ba0312e4
Fred Mitchell <fred.mitchell@gmx.de>
2016-12-17 14:40:26 +0100

    adding a daemon interface

--- lib/rubyneat/cli/daemon.rb
+++ lib/rubyneat/cli/daemon.rb
@@ -4,11 +4,18 @@ module NEAT
       desc 'background', 'Run RubyNEAT Daemon in the background'
       def background
         puts "Daemon Lives in the Background"
+        daemon
       end
     
       desc 'foreground', 'Run RubyNEAT Daemon in the foreground, with STDOUT/STDERR. Interrupt with ^C.'
       def foreground
-        puts "Cthulhu woke up"
+        daemon
+      end
+      
+      no_tasks do
+        def daemon
+          puts "Cthulhu woke up"
+        end
       end
     end
   end
