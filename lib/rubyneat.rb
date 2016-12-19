62709679dee8b96879b2574db9a7009073c8e61e
Fred Mitchell <fred.mitchell@gmx.de>
2016-12-19 21:25:32 +0100

    deep debugging in progress

--- lib/rubyneat.rb
+++ lib/rubyneat.rb
@@ -4,7 +4,12 @@ require 'awesome_print'
 require 'process/daemon'
 
 set_trace_func proc { |event, file, line, id, binding, classname|
-  printf "%8s %s:%-2d %10s %8s\n", event, file, line, id, classname
+  if /ruby24.rb/ =~ file
+    printf "%8s %s:%-2d %10s %8s\n", event, file, line, id, classname
+    ap binding.local_variables
+        .select{|v| [:str,:idx].member?(v)  }
+        .map{|v| [v, binding.local_variable_get(v)]}.to_h
+  end
 }
 
 require_relative 'rubyneat/rubyneat'
